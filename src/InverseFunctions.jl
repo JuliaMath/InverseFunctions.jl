105a559a8d933a55f940ae5057a9de822f64f0b1
David Widmann <devmotion@users.noreply.github.com>
2021-10-09 11:37:25 +0200

    Clean InverseFunctions.jl

--- src/InverseFunctions.jl
+++ src/InverseFunctions.jl
@@ -1,13 +1,12 @@
 # This file is a part of InverseFunctions.jl, licensed under the MIT License (MIT).
-
-__precompile__(true)
-
 """
     InverseFunctions
 
 Lightweight package that defines an interface to invert functions.
 """
 module InverseFunctions
+
+export inverse
 
 include("inverse.jl")
 
