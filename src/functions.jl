de31094b9b930ae3ca56097a37f9889089685227
Oliver Schulz <oschulz@mpp.mpg.de>
2022-03-08 15:48:51 +0100

    Restrict domain of square (#9)
    Co-authored-by: David Widmann <david.widmann@it.uu.se>

--- src/functions.jl
+++ src/functions.jl
@@ -1,8 +1,12 @@
 # This file is a part of InverseFunctions.jl, licensed under the MIT License (MIT).
 
 """
-    square(x)
+    square(x::Real)
 
 Inverse of `sqrt(x)` for non-negative `x`.
 """
 square(x) = x^2
+function square(x::Real)
+    x < zero(x) && throw(DomainError(x, "`square` is defined as the inverse of `sqrt` and can only be evaluated for non-negative values"))
+    return x^2
+end
