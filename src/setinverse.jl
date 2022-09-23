c84aa3674fc60703352749e0c6e62b14d0281682
Oliver Schulz <oschulz@mpp.mpg.de>
2022-09-23 16:42:31 +0200

    Fix docstring of setinverse
    Co-authored-by: Chad Scherrer <chad.scherrer@gmail.com>

--- src/setinverse.jl
+++ src/setinverse.jl
@@ -31,7 +31,7 @@ range that is guaranteed by the use case but not in general.
 
 For example, `asin` not is a valid inverse of `sin` for arbitrary arguments
 of `sin`, but can be a valid inverse if the use case guarantees that the
-argument of `sin` will always be within `-π` and `π`:`
+argument of `sin` will always be within `-π` and `π`:
 
 ```jldoctest
 julia> foo = setinverse(sin, asin);
